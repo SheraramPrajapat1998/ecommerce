b973a1c5ee875121c5bb36641c6f7acb5232f05f
SheraramPrajapat1998 <sheraramprajapat1998@gmail.com>
2020-11-01 21:36:14 +0530

    generate PDF invoice for each order functionality added in admin site

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ecommerce
+
+
+### 1.  products
+    - Category
+    - Product
+
+### 2.  cart
+    - cart functionality(add or remove product and cart detail)
+    - cart available to all templates (context_processors)
+  
+### 3.  orders
+    - Order
+    - OrderItem
+    ** Admin site functionality **  - order detail
+                                    - export to CSV
+                                    - pdf view
--- src/orders/admin.py
+++ src/orders/admin.py
@@ -30,18 +30,22 @@ def export_to_csv(modeladmin, request, queryset):
       data_row.append(value)
     writer.writerow(data_row)
   return response
-export_to_csv.shortdescription = 'Export to CSV'
+export_to_csv.short_description = 'Export to CSV'
 
 
 def order_detail(obj):
   url = reverse('orders:admin_order_detail', args=[obj.id])
   return mark_safe(f'<a href="{ url }">View</a>')
 
+def order_pdf(obj):
+  url = reverse('orders:admin_order_pdf', args=[obj.id])
+  return mark_safe(f'<a href="{ url }">PDF</a>')
+order_pdf.short_description = 'Invoice'
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
   list_display = ['id', 'first_name', 'last_name', 'email',
   'address', 'postal_code', 'city', 'paid',
-  'created', 'updated', order_detail]
+  'created', 'updated', order_detail, order_pdf]
   list_filter = ['paid', 'created', 'updated']
   inlines = [OrderItemInline]
   actions = [export_to_csv]
